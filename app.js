4af3fbdb871289c901601960ba8171135a948fab
1560652 <37291387+1560652@users.noreply.github.com>
2018-06-02 19:44:57 +0700

    doi mk

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var pg = require('pg');
 var config = {
     user: 'postgres',
     database: 'daugia', 
+    password: '123456', 
     port: 5432, 
     max: 10, // max number of connection can be open to database
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
